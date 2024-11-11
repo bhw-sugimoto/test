046a5e552ffed44140be862330b594aef75d5176
bhw_sugimoto <t.sugimoto@bhworks.com>
2024-11-11 13:42:44 +0900

    出力先を変更

--- generate_diff_txt.bat
+++ generate_diff_txt.bat
@@ -6,10 +6,13 @@ set REPO=%1
 set FILE=%2
 set SHA=%3
 
-:: 差分を保存するファイル名を設定
-set diff_file=%REPO%\diff_%SHA%.txt
+:: 出力先ディレクトリ（絶対パスを指定）
+set output_dir=%USERPROFILE%\Desktop\GitDiffs
 
-:: 差分を取得してテキストファイルに保存
+:: 差分を保存するファイル名を設定
+set diff_file=%output_dir%\diff_%SHA%.txt
+
+:: リポジトリに移動して差分を取得
 git -C %REPO% diff %SHA^ %SHA% -- %FILE% > %diff_file%
 
 :: 結果を表示
